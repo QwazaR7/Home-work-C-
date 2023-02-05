fcce65ef9a77433f26cf6c53ccb89bb96e421c88
Quasar <puzyrkovv@yandex.ru>
2023-02-05 15:47:46 +0700

    Added lesson about array

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -36,6 +36,7 @@ while(a < 0);
 */
 
 //Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+/*
 int DigitCount(int number)
 {
     int count = 0;
@@ -49,14 +50,16 @@ int DigitCount(int number)
 Console.Write("Enter number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Number of digits is: " + DigitCount(num));
+*/
 
 //Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
 
+/*
 int Factorial(int number)
 {
     int result = 1;
     /* for(int i = 1; i <= number; i++)
-        result *= i; */
+        result *= i; /
         
     while(number > 1)  result *= number--;
        // number--;
@@ -66,3 +69,31 @@ int Factorial(int number)
 Console.Write("Enter number N: ");
 //int numN = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Factorial is: " + Factorial(Convert.ToInt32(Console.ReadLine())));
+*/
+// Методы работы с массивами. Настройка генерации и заполнения.
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+Console.Write("Input a length of new array: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(length,min,max);
+ShowArray(myArray);
